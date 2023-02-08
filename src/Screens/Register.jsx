150d5fc719c312c01946448e1bfe2d2bf19718df
chethika10 <chethika10@gmail.com>
2023-02-08 17:47:35 +0530

    register page without css

--- src/Screens/Register.jsx
+++ src/Screens/Register.jsx
@@ -1,7 +1,25 @@
 export const Regiser =()=>{
     return(
         <>
-        login
+        <form >
+            <label htmlFor="name">Name</label>
+            <input type="text"  id="name" name="name"/>
+
+            <label htmlFor="date">Birthday</label>
+            <input type="date" id="Birthday" name="Birthday" />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="example@example.com" id="email" name="email"/>
+
+            <label htmlFor="userName">UserName</label>
+            <input type="text" placeholder="UserName" id="userName" name="userName"/>
+
+            <label htmlFor="password">Password</label>
+            <input type="password" placeholder="*******" id="password" name="password"/>
+
+            <label htmlFor="Role">Role</label>
+            <input type="text" id="role" name="role"/>
+        </form>
         </>
     )
 }
